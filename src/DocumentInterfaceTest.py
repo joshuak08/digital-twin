bcb2d46e35382ec5f0e7e846dc8d52599c51dd10
AmosH <vo21765@bristol.ac.uk>
2022-11-14 15:56:07 +0000

    started writing unit tests for interface creation

--- src/DocumentInterfaceTest.py
+++ src/DocumentInterfaceTest.py
@@ -1,4 +1,5 @@
 import unittest
+import DocumentInterface
 
 
 class TestDocument:
@@ -74,6 +75,45 @@ class TestDefinition:
 
     def __init__(self, name):
         self.Name = name
+
+
+def elementTypeElement(elementId):
+    return TestElement(True, "Generic", elementId, [])
+
+def elementOfCategory(category, elementId):
+    return TestElement(False, category, elementId, [])
+
+def generic3CatDocument(cat1, cat2, cat3):
+    currentId = 0
+    elements = []
+    for i in range(3):
+        elements.append(elementOfCategory(cat1, currentId))
+        currentId += 1
+    for i in range(2):
+        elements.append((elementOfCategory(cat2, currentId)))
+        currentId += 1
+    elements.append(elementOfCategory(cat3, currentId))
+    return TestDocument(elements)
+class DocumentInterfaceTests(unittest.TestCase):
+
+    def test_no_element_types(self):
+        element = elementTypeElement(0)
+        document = TestDocument([element])
+        collector = TestCollector(document.elementList)
+        interface = DocumentInterface.DocumentInterface(document, collector, [])
+        self.assertEqual(len(interface.elementDict), 0)
+
+    def test_category_filter(self):
+        cat1 = "Category1"
+        cat2 = "Category2"
+        cat3 = "Category3"
+        document = generic3CatDocument(cat1, cat2, cat3)
+        collector = TestCollector(document.elementList)
+        interface = DocumentInterface.DocumentInterface(document, collector, [cat1, cat2])
+        self.assertEqual(len(interface.elementDict), 2)
+        #do some more assertions (correct list lengths)
+
+
 
 
 if __name__ == '__main__':
