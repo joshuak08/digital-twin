f30698893436e617ad9523483feaa54797bfe0e8
Amos <vo21765@bristol.ac.uk>
2022-11-15 09:56:36 +0000

    even more tests

--- src/DocumentInterfaceTest.py
+++ src/DocumentInterfaceTest.py
@@ -77,27 +77,31 @@ class TestDefinition:
         self.Name = name
 
 
-def elementTypeElement(elementId):
+def element_type_element(elementId):
     return TestElement(True, "Generic", elementId, [])
 
-def elementOfCategory(category, elementId):
+
+def element_of_category(category, elementId):
     return TestElement(False, category, elementId, [])
 
-def generic3CatDocument(cat1, cat2, cat3):
+
+def generic_3_cat_document(cat1, cat2, cat3):
     currentId = 0
     elements = []
     for i in range(3):
-        elements.append(elementOfCategory(cat1, currentId))
+        elements.append(element_of_category(cat1, currentId))
         currentId += 1
     for i in range(2):
-        elements.append((elementOfCategory(cat2, currentId)))
+        elements.append((element_of_category(cat2, currentId)))
         currentId += 1
-    elements.append(elementOfCategory(cat3, currentId))
+    elements.append(element_of_category(cat3, currentId))
     return TestDocument(elements)
+
+
 class DocumentInterfaceTests(unittest.TestCase):
 
     def test_no_element_types(self):
-        element = elementTypeElement(0)
+        element = element_type_element(0)
         document = TestDocument([element])
         collector = TestCollector(document.elementList)
         interface = DocumentInterface.DocumentInterface(document, collector, [])
@@ -107,23 +111,34 @@ class DocumentInterfaceTests(unittest.TestCase):
         cat1 = "Category1"
         cat2 = "Category2"
         cat3 = "Category3"
-        document = generic3CatDocument(cat1, cat2, cat3)
+        document = generic_3_cat_document(cat1, cat2, cat3)
         collector = TestCollector(document.elementList)
         interface = DocumentInterface.DocumentInterface(document, collector, [cat1, cat2])
         self.assertEqual(len(interface.elementDict), 2)
-        self.assertEqual(len(interface.elementDict[cat1]), 3)
-        self.assertEqual(len(interface.elementDict[cat2]), 2)
+        self.assertEqual(len(interface.get_elements_of_name(cat1)), 3)
+        self.assertEqual(len(interface.get_elements_of_name(cat2)), 2)
 
     def test_empty_categories(self):
         cat1 = "Category1"
         cat2 = "Category2"
         cat3 = "Category3"
-        document = generic3CatDocument(cat1, cat2, cat3)
+        document = generic_3_cat_document(cat1, cat2, cat3)
         collector = TestCollector(document.elementList)
         interface = DocumentInterface.DocumentInterface(document, collector, [])
         self.assertEqual(len(interface.elementDict), 3)
 
+    def test_has_parameter(self):
+        element = TestElement(False, "Generic", 0, [TestParameter("Height", None, 10)])
+        interface = DocumentInterface.ElementInterface(element)
+        self.assertTrue(interface.has_parameter("Height"))
 
+    def test_parameter_has_right_value(self):
+        element = TestElement(False, "Generic", 0, [TestParameter("Height", None, 10), TestParameter("Material", "Steel", None)])
+        interface = DocumentInterface.ElementInterface(element)
+        parameter1 = interface.get_parameter("Height")
+        parameter2 = interface.get_parameter("Material")
+        self.assertEqual(parameter1.numericalValue, 10.0)
+        self.assertEqual(parameter2.stringValue, "Steel")
 
 
 if __name__ == '__main__':
