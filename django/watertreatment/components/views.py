87c7c015f29d358112f713941f89f4cf1bc44a34
rlarg <pharadonlarg@gmail.com>
2022-11-15 12:54:16 +0000

    Rollback bootstrap

--- django/watertreatment/components/views.py
+++ django/watertreatment/components/views.py
@@ -2,32 +2,48 @@
 from django.shortcuts import render
 
 posts = [
-	{ 
-		'name' : 'firstComponent',
-		'source' : 'pyRevit',
-		'width' : 'w',
-		'height' : 'h',
-		'length' : 'l',
-		'content' : 'first content',
-		'codenum' : '#1'
-	 },
-	 { 
-		'name' : 'secondComponent',
-	 	'source' : 'pyRevit',
-		'width' : '3',
-		'height' : '4',
-		'length' : '8',
-		'content' : 'second content',
-		'codenum' : '#2'
-	 }
+    {
+        'name': 'firstComponent',
+        'source': 'pyRevit',
+        'width': 'w',
+        'height': 'h',
+        'length': 'l',
+        'content': 'first content',
+    },
+    {
+        'name': 'secondComponent',
+        'source': 'pyRevit',
+        'width': '3',
+        'height': '4',
+        'length': '8',
+        'content': 'second content',
+    },
+    {
+        'name': 'thirdComponent',
+        'source': 'pyRevit',
+        'width': '3',
+        'height': '4',
+        'length': '8',
+        'content': 'third content',
+    },
+    {
+        'name': 'foruthComponent',
+        'source': 'pyRevit',
+        'width': '3',
+        'height': '4',
+        'length': '8',
+        'content': 'fourth content',
+    }
 ]
+
 
 def home(request):
     # return HttpResponse('<h1>Water Treatment Components</h1>')
-	context = {
-		'posts' : posts
-	}
-	return render(request, 'components/home.html', context)
+    context = {
+        'posts': posts
+    }
+    return render(request, 'components/home.html', context)
+
 
 def about(request):
-    return render(request, 'components/about.html', {'title' : 'about'})
+    return render(request, 'components/about.html', {'title': 'about'})
