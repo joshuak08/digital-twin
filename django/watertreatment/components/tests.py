68ab64db4a43cf1e75a7577942a06a4e824ca8e8
rlarg <pharadonlarg@gmail.com>
2022-11-23 16:39:52 +0000

    Small Test for Homepage

--- django/watertreatment/components/tests.py
+++ django/watertreatment/components/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+
+class TestView(TestCase):
+
+    def testHome(self):
+        url = '/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+       
