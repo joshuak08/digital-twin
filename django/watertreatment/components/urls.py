baf13bbda82254b3e0135f1c788edc09ad31bc7c
rlarg <pharadonlarg@gmail.com>
2022-11-23 16:10:59 +0000

    Formatting components into Grids and a static JS image

--- django/watertreatment/components/urls.py
+++ django/watertreatment/components/urls.py
@@ -4,5 +4,6 @@ from . import views
 # Map url to function in views module
 urlpatterns = [
     path('', views.home, name="components-home"),
-    path('about/', views.about, name="components-about")
+    path('about/', views.about, name="components-about"),
+    path('time/', views.time, name="components-time")
 ]
--- django/watertreatment/components/views.py
+++ django/watertreatment/components/views.py
@@ -1,30 +1,70 @@
 # from curses.ascii import HT
 from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
 
 # Objects data we want to display
 posts = [
     {
-        'name': 'firstComponent',
-        'source': 'pyRevit',
+        'name': 'First Component',
+        'content': 'First content',
         'params':
             {
                 'Width': 'w',
                 'Height': 'h',
                 'Length': 'l',
             },
-        'content': 'first content',
     },
     {
-        'name': 'secondComponent',
-        'source': 'pyRevit',
+        'name': 'Second Component',
+        'content': 'Second content',
         'params':
             {
                 'Width': 1,
                 'Height': 2,
                 'Length': 3,
             },
-        'content': 'first content',
-    }
+    },
+    {
+        'name': 'Third Component',
+        'content': 'Third content',
+        'params':
+            {
+                'Width': 1,
+                'Height': 2,
+                'Length': 3,
+            },
+    },
+    {
+        'name': 'Fourth Component',
+        'content': 'Fourth content',
+        'params':
+            {
+                'Width': 1,
+                'Height': 2,
+                'Length': 3,
+            },
+    },
+    {
+        'name': 'Dummy  Component',
+        'content': 'Baka content',
+        'params':
+            {
+                'Width': 1,
+                'Height': 2,
+                'Length': 3,
+            },
+    },
+    {
+        'name': 'Dummy  Component',
+        'content': 'Baka content',
+        'params':
+            {
+                'Width': 1,
+                'Height': 2,
+                'Length': 3,
+            },
+    },
 
 ]
 
@@ -39,3 +79,11 @@ def home(request):
 
 def about(request):
     return render(request, 'components/about.html', {'title': 'about'})
+
+
+def time(request):
+    now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    context = {
+        'curr_time': now
+    }
+    return render(request, 'components/time.html', context)
