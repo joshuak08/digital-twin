0af6bda8e76e509c17712d20b328e993edcea1ce
rlarg <pharadonlarg@gmail.com>
2022-11-24 22:04:37 +0000

    Test Post Creation

--- django/watertreatment/components/tests.py
+++ django/watertreatment/components/tests.py
@@ -1,7 +1,20 @@
 from django.test import TestCase
+from .models import Post
 
 
 # Create your tests here.
+
+class TestPosts(TestCase):
+
+    def testCreatePost(self):
+        name = "TestName"
+        postID = 123
+        content = "TestContent"
+        params = {'TestParam': 456}
+        post1 = Post(name, postID, content, params)
+        post2 = Post(name, postID, content, params)
+        self.assertEqual(post1, post2)
+
 
 class TestView(TestCase):
 
@@ -9,5 +22,3 @@ class TestView(TestCase):
         url = '/'
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
-
-       
