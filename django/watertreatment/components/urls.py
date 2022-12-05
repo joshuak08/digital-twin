a288cfc2890b36026bc2fcba193bf23f5922462f
AmosH <vo21765@bristol.ac.uk>
2022-12-05 14:10:57 +0000

    Started multiple pages work

--- django/watertreatment/components/urls.py
+++ django/watertreatment/components/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name="components-home"),
     path('about/', views.about, name="components-about"),
+    path('elements/', views.elements, name="components-elements"),
+    path('elements/<str:type>/', views.elementsOfType, name="components-elements-of-type"),
+    path('elements/<str:type>/<str:elementID>/', views.element, name="component-element"),
+    path('revit-model/', views.revitModel, name="components-revit-model")
 ]
--- django/watertreatment/components/views.py
+++ django/watertreatment/components/views.py
@@ -16,3 +16,23 @@ def home(request):
 
 def about(request):
     return render(request, 'components/about.html', {'title': 'About'})
+
+def elements(request):
+    types = {"pipe": 33, "sandfilter": 4, "electrics": 7}
+    context = {'elements': "Wazaaa", 'title': "Elements", "types": types}
+
+    return render(request, 'components/elements.html', context)
+
+def elementsOfType(request, type):
+    elements = {"pipe": ["12", "445", "354"], "sandfilter": ["55"], "electrics" : ["49"]}
+    context = {"type": type, "elements": elements[type]}
+    return render(request, 'components/elements-of-type.html', context)
+
+def element(request, type, elementID):
+    params = {"name": "default", "height": 12, "volume": 23}
+    context = {"elementID": elementID, "params": params, "type": type}
+    return render(request, 'components/element.html', context)
+
+def revitModel(request):
+    return render(request, 'components/revit-model.html')
+
