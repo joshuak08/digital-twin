b3d02c31bd39d61df2b5aca73940f72448d973b6
AmosH <vo21765@bristol.ac.uk>
2022-12-06 16:25:06 +0000

    Removing about page

--- django/watertreatment/components/urls.py
+++ django/watertreatment/components/urls.py
@@ -4,7 +4,6 @@ from . import views
 # Map url to function in views module
 urlpatterns = [
     path('', views.home, name="components-home"),
-    path('about/', views.about, name="components-about"),
     path('types/', views.elements, name="components-types"),
     path('types/<str:type>/', views.elementsOfType, name="components-elements-of-type"),
     path('types/<str:type>/<int:ID>/', views.element, name="components-elements-of-type"),
