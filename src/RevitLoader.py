a2a29bec51cfac9be6035c4804125855d26fd672
Leo_Lai <leojacklai@outlook.com>
2022-12-06 15:52:55 +0000

    databaser class now returns in json format

--- src/RevitLoader.py
+++ src/RevitLoader.py
@@ -1,6 +1,7 @@
 import Autodesk.Revit.DB as DB
 from revitutils import doc
 import DocumentInterface
+import sqliteRevit
 
 collector = DB.FilteredElementCollector(doc)
 
@@ -8,5 +9,9 @@ categories = []
 # Construct categories - user input / hardcode / something else
 
 docI = DocumentInterface.DocumentInterface(doc, collector, categories)
-
 # Next needs to pass this into something to construct the database, and that should be it
+
+dataBaser = sqliteRevit.DataBaser(docI)
+dataBaser.access_table()
+dataBaser.store_elems()
+
--- src/sqliteRevit.py
+++ src/sqliteRevit.py
@@ -1,16 +1,17 @@
-import dummyRevitDoc
+import sqlTest
 import sqlite3
 import array
+import json
 
 
 class DataBaser:  # class to hold info about database
-    def __init__(self, DocumentInterface):
-        self.document = DocumentInterface   # assigns document to a variable
+    def __init__(self, document_interface):
+        self.document = document_interface  # assigns document to a variable
         self.dbConn = sqlite3.connect("document.db")  # represents connection to db
         self.cursor = self.dbConn.cursor()  # database cursor
 
     # creates a table for elements if it doesn't already exist
-    def create_elem_table(self):
+    def access_table(self):
         self.cursor.execute("""CREATE TABLE IF NOT EXISTS document (
                                 elemID integer, 
                                 name text, 
@@ -23,34 +24,40 @@ class DataBaser:  # class to hold info about database
     def store_elems(self):
         for element in self.document.elementList:  # iterates through elements in document and adds them to db
             parameters = element.Parameters
-            param_string = parameters.Name + ',' + parameters.stringVal + ',' + str(parameters.doubleVal) + ',' + str(parameters.HasValue)
+            param_string = "{name:" + parameters.Definition.Name + ",stringVal:" + parameters.stringVal \
+                           + ",doubleVal:" + str(parameters.doubleVal) + ",HasVal:" + str(parameters.HasValue) + "}"
+
             self.cursor.execute("INSERT INTO document VALUES (?, ?, ?, ?)", (element.Id,
                                                                              element.Name,
                                                                              element.category,
                                                                              param_string))
         self.dbConn.commit()  # commits changes
 
-    # returns entire table
+    # returns entire table in json format
     def return_elems(self):
         table = self.cursor.execute("SELECT * FROM document")
-        return table
+        dict_arr = [dict((self.cursor.description[i][0], row[i])  # array of dictionaries
+                         for i, value in enumerate(row))  # late binding, enumerate allows rows to be iterable
+                    for row in self.cursor.fetchall()  # fetchall returns rows as a list of tuples (1 row = 1 tuple)
+                    ]
+        json_out = json.dumps(dict_arr)
+        return json_out
 
-# params = dummyRevitDoc.TestParameter("param", "string", 0.1)  # initialises params
-# elem1 = dummyRevitDoc.element_type_element(0, params)  # initialises an element
-# elem2 = dummyRevitDoc.element_type_element(1, params)  # initialises an element
-# document = dummyRevitDoc.TestDocument([elem1, elem2])  # initialises a document
 
+# params = sqlTest.TestParameter("param", "string", 0.1)  # initialises params
+# elem1 = sqlTest.element_type_element(0, params)  # initialises an element
+# # elem2 = sqlTest.element_type_element(1, params)  # initialises an element
+# document = sqlTest.TestDocument([elem1])  # initialises a document
+#
 # database = DataBaser(document)  # creates databaser class with the document passed in
-# database.create_elem_table()  # creates a table with element columns as parameters if one doesn't exist
-#
+# database.access_table()  # creates a table with element columns as parameters if one doesn't exist
+# #
 # database.store_elems()
-# print(database.cursor.execute("SELECT * FROM document").fetchall())  # queries the database and prints all rows
-#
+# print(database.return_elems())  # queries the database and prints all rows
+# #
 # database.cursor.execute("DELETE FROM document WHERE elemID=1")  # deletes the element with elemID = 1
-# database.dbConn.commit()    # this commits changes to the database
-#
-# print(database.cursor.execute("SELECT * FROM document").fetchall())  # queries database for all rows and columns and prints them
-#
+# database.dbConn.commit()  # this commits changes to the database
+# #
 # database.cursor.execute("DELETE FROM document")  # empties the database
 # database.dbConn.commit()  # commits changes
 # database.dbConn.close()  # closes connection to database
