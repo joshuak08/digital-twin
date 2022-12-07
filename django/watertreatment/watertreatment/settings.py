fe0adc45e33d67334bd17ac50658112071c67edf
AmosH <vo21765@bristol.ac.uk>
2022-12-07 12:26:15 +0000

    Adding actual database to the server

--- django/watertreatment/watertreatment/settings.py
+++ django/watertreatment/watertreatment/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'watertreatment.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'testdocument.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     },
 
 }
