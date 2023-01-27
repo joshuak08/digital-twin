34282a756991d38faa3e72c93e922636c1498491
Leo_Lai <leojacklai@outlook.com>
2023-01-27 11:39:45 +0000

    simulation branch

---
+++ simluation/genericPipe.py
@@ -0,0 +1,15 @@
+class genericPipe:
+    def __init__(self, inputs, outputs, length):
+        self.inputs = inputs
+        self.outputs = outputs
+        self.length = length
+        self.maxVolume = None
+        self.capacity = 0
+
+    def push(self):
+        pass
+
+    def snapshot(self):
+        pass
+
+
