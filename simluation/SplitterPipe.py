5532af3981e3fe62a35d1252e407c420ebeffb19
Leo_Lai <leojacklai@outlook.com>
2023-01-30 01:22:21 +0000

    fleshed out push and snapshot functions for SplitterPipe, still need to fix how it handles time, any suggestions to improve functionallity is welcome

--- simluation/SplitterPipe.py
+++ simluation/SplitterPipe.py
@@ -1,19 +1,36 @@
 from GenericPipe import GenericPipe
 
 
+# Q = av where Q is the flow rate, a = cross-sectional area, v = velocity of water
+
 class SplitterPipe(GenericPipe):
-    def __init__(self, inputs, outputs, length):
-        super().__init__(inputs, outputs, length)
+    def __init__(self, id_num, inputs, outputs, length):
+        super().__init__(id_num, inputs, outputs, length)
 
     # ======== override methods ======== #
-    def push(self):
+    def push(self, flow_in, time):
         # TODO
-        # will push water down all children connections (outputs) split equally between children
-        # cascading effect down children
-        pass
+        # THIS IS JUST EXAMPLE CODE
+        # currently DOES NOT DO TIME PROPERLY, we need all components to keep track of the oldest time (perhaps by pointer)
 
-    def snapshot(self):
+        self.time = max(time, self.time) + 1  # increases time by 1 after a push
+        self.capacity = self.capacity + flow_in - self.outputRate*len(self.outputs)  # increase capacity difference of input and output
+
+        if self.capacity > self.maxVolume:
+            raise Exception("capacity is greater than max volume :(")
+
+        for child_pipe in self.outputs:       # iterates through all output pipes
+            self.time = max(self.time, child_pipe.push(self.outputRate, self.time))  # updates current time with oldest time and pushes flow
+    # ================================== #
+
+    def snapshot(self, snap_dict):
         # TODO
-        # maybe return a list of tuples [(component, [attributes])] via recursively calling into children
-        pass
+        # currently incorrect, we want to take the largest time of any component after a push is done
+        # returns a dictionary of pipe id's as keys with their value being a tuple of time and capacity
+        snap_dict[self.id_num] = (self.time, self.capacity)  # adds self to dictionary with time and capacity
+
+        for child_pipe in self.outputs:
+            child_pipe.snap(snap_dict)
+
+        return snap_dict
     # ================================== #
