35df9fda9d82184b28421dceada0f6e6a358d4ae
rlarg <pharadonlarg@gmail.com>
2023-02-01 18:12:21 +0000

    Pipe animation

---
+++ django/watertreatment/components/static/animation.js
@@ -0,0 +1,30 @@
+class WaterBar {
+	constructor(x, y, w, h, maxCapacity, colour) {
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+		this.maxCapacity;
+		this.maxHeight = h;
+		this.colour = colour;
+	}
+}
+
+	// // Grey pipes
+	// ctx.strokeStlye = 'grey';
+	// ctx.strokeRect(130, 180, 20, 20);
+	// ctx.strokeRect(250, 180, 20, 20);
+	// ctx.strokeRect(370, 180, 20, 20);
+
+	// // Blue pipes
+	// ctx.strokeStyle = "blue";
+	// ctx.strokeRect(110, 110, 20, 100);
+	// ctx.strokeRect(230, 110, 20, 100);
+	// ctx.strokeRect(350, 110, 20, 100);
+	// ctx.strokeRect(470, 110, 20, 100);
+
+	// // Purple pipes
+	// ctx.strokeStyle = "purple";
+	// ctx.strokeRect(150, 180, 80, 20);
+	// ctx.strokeRect(270, 180, 80, 20);
+	// ctx.strokeRect(390, 180, 80, 20);
--- django/watertreatment/components/static/script.js
+++ django/watertreatment/components/static/script.js
@@ -1,43 +1,115 @@
 const canvas = document.getElementById('canvas');
-// Tanks
+const width = canvas.width;
+const height = canvas.height;
+const NUMBER_OF_TANKS = 4;
+const NUMBER_OF_PIPES = 3;
 const ctx = canvas.getContext('2d');
-ctx.fillStyle = 'grey';
-ctx.fillRect(30, 50, 100, 200);
-ctx.fillRect(150, 50, 100, 200);
-ctx.fillRect(270, 50, 100, 200);
-ctx.fillRect(390, 50, 100, 200);
 
-// Roofs
-for (let i = 0; i < 4; i++) {
-	let dx = 120;
-	change_in_x = (dx * i) 
-	ctx.beginPath();
-	ctx.moveTo(30 + change_in_x, 250);
-	ctx.lineTo(80 + change_in_x, 300);
-	ctx.lineTo(130 + change_in_x, 250);
-	ctx.closePath();
-	ctx.fillStyle = 'grey';
-	ctx.fill();
-	ctx.stroke();
+class Pipe {
+	constructor(x, y, w, h, colour) {
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+		this.colour = colour;
+	}
+
+	render() {
+		ctx.strokeStyle = this.colour;
+		ctx.lineWidth = 3;
+		ctx.strokeRect(this.x, this.y, this.w, this.h);
+	}
 }
 
-// Grey pipes
-ctx.strokeStlye = 'grey';
-ctx.strokeRect(130, 180, 20, 20);
-ctx.strokeRect(250, 180, 20, 20);
-ctx.strokeRect(370, 180, 20, 20);
+class WaterPipe extends Pipe {
+	constructor(x, y, w, h, colour, fillHeight) {
+		super(x, y, w, h, colour);
+		this.fillHeight = fillHeight;
+	}
 
-// Blue pipes
-const ctx2 = canvas.getContext("2d");
-ctx2.strokeStyle = "blue";
-ctx.strokeRect(110, 110, 20, 100);
-ctx.strokeRect(230, 110, 20, 100);
-ctx.strokeRect(350, 110, 20, 100);
-ctx.strokeRect(470, 110, 20, 100);
+	render() {
+		ctx.fillStyle = this.colour;
+		ctx.fillRect(this.x, this.y, this.w, this.h);
+	}
+} 
 
-// Purple pipes
-const ctx3 = canvas.getContext("2d");
-ctx3.strokeStyle = "purple";
-ctx3.strokeRect(150, 180, 80, 20);
-ctx3.strokeRect(270, 180, 80, 20);
-ctx3.strokeRect(390, 180, 80, 20);
+
+let fillLevel = 0; // between 0 and 1	
+
+const drawScene = () => {
+
+	// Tanks
+	for (let i = 0; i < NUMBER_OF_TANKS; i++) {
+		let dx = 120; 
+		change_in_x = (dx * i);
+		ctx.fillStyle = 'grey';
+		ctx.fillRect(30 + change_in_x, 50, 100, 200);
+		// ctx.fillStyle = 'Black';
+		// ctx.fillRect(30 + change_in_x, 50, 100, 200);
+	}
+
+	// Roofs
+	for (let i = 0; i < NUMBER_OF_TANKS; i++) {
+		let dx = 120;
+		change_in_x = (dx * i) 
+		ctx.beginPath();
+		ctx.moveTo(30 + change_in_x, 250);
+		ctx.lineTo(80 + change_in_x, 300);
+		ctx.lineTo(130 + change_in_x, 250);
+		ctx.closePath();
+		ctx.strokeStyle = 'black';
+		ctx.fill();
+		ctx.stroke();
+	}
+
+	// Filling pipe
+	// ctx.fillStyle = "#afeeee";
+	// ctx.fillRect(110, 210 - fillHeight, 18, fillHeight);
+	
+	// All pipes
+	let pipes = []
+	let waterPipes = []
+	
+	// Three pipes
+	for (let i = 0; i < NUMBER_OF_PIPES; i++) {
+		let change_in_x = 120;
+		pipes.push(
+			new Pipe(130 + (change_in_x * i) , 180, 20, 20, 'blue'), 
+			new Pipe(150 + (change_in_x * i) , 180, 80, 20, 'purple'), 
+		);
+	}
+	
+	let height = 100;
+	let fillHeight = height * fillLevel;
+	// const waterPipe = new WaterPipe(110, 210 - fillHeight, 18, fillHeight , "#afeeee");
+	
+	// Four pipes
+	for (let i = 0; i < NUMBER_OF_TANKS; i++) {
+		let change_in_x = 120;
+		pipes.push(
+			new WaterPipe(110 + (change_in_x * i) , 210 - fillHeight, 20, fillHeight , "#afeeee"),
+			new Pipe(110 + (change_in_x * i), 110, 20, 100, '#000000')
+		);
+	}
+	
+	
+	for (let i = 0 ; i < pipes.length; i++ ) {
+		pipes[i].render();
+	}
+		
+}
+
+const renderLoop = () => {
+	// Render background
+	ctx.clearRect(0, 0, width, height);
+	
+	drawScene();
+	
+	fillLevel += 0.005;
+	console.log(fillLevel);
+	if (fillLevel > 1) { fillLevel = 0;}
+	
+	requestAnimationFrame(renderLoop);
+};
+
+renderLoop();
