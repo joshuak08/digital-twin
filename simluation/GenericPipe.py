d9e3700e28ff82f697004b6b69eb99d6ab2712c5
AmosH <vo21765@bristol.ac.uk>
2023-02-08 16:16:16 +0000

    removed time from pipe and pipe splitter, and added a snap_num parameter to snapshot

--- simluation/GenericPipe.py
+++ simluation/GenericPipe.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 
 
 class GenericPipe(ABC):
-    def __init__(self, id_num, inputs, outputs, length):
+    def __init__(self, id_num, inputs, outputs, length, tickLength):
         self.id_num = id_num
         self.inputs = inputs  # inputs of pipe
         self.outputs = outputs  # outputs of pipe
@@ -10,15 +10,15 @@ class GenericPipe(ABC):
         self.outputRate = sum(inputX.outputRate for inputX in inputs) / outputs  # sum of all input rates divided by outputs
         self.maxVolume = 8   # maximum volume of pipe m^3
         self.capacity = 0       # current capacity of pipe
-        self.time = 0
         self.valve = False
+        self.tickLength = tickLength # length of each round in seconds
 
     @abstractmethod
-    def push(self, flowi_in, time: int) -> int:  # pushes water down pipe on a single tick
+    def push(self, flowi_in) -> int:  # pushes water down pipe on a single tick
         pass
 
     @abstractmethod
-    def snapshot(self, snap_dict: dict) -> dict:  # takes snapshot of pipe and stores it in a db
+    def snapshot(self, snap_dict: dict, snap_num: int) -> dict:  # takes snapshot of pipe and stores it in a db
         pass
 
     def toggle_valve(self):
