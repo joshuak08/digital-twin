24d1761b797eb3aaafebfdbb2cf2a31a0814be47
AmosH <vo21765@bristol.ac.uk>
2023-02-13 15:47:05 +0000

    Added proper backwash features to SandFilters, to allow for proper use within the system

--- simulation/SandFilter.py
+++ simulation/SandFilter.py
@@ -13,18 +13,25 @@ class SandFilter(GenericPipe.GenericPipe):
         self.backwash_pipe = outputs[1] # pipe to output wash water
         self.output = self.normal_pipe # current output of the tank
         self.height = length # height of the tank
-        self.sand_height = self.height / 2 # arbitrary, current height of sand in the tank
+        self.particulate_mass = 0 # mass of particulate currently captured in the sand
         self.backwash = False # whether or not the tank is currently backwashing
         self.max_volume = 100 # max volume of the tank
         self.valve = False # whether or not the tank is shut
         self.capacity = 0 # current volume of liquid in the tank
-        self.radius = 5 # arbitrary
+        self.radius = radius # radius of the tank in metres
         self.tick_length = tick_length # length of each round in seconds
         self.radius = radius
         # will likely be pre-set, user just decides how often a snapshot is taken
 
         self.id = id_num # id of the component, used for snapshotting
         
+        self.backwash_timer = 0
+        # Numbers for a standard Sand Filter
+        # Diameter = 3m
+        # Height of tank area ~= 8m (ish)
+        # Hydraulic Capacity = 71 m^3 / hr (71,000 litres)
+        # Particulate (TSS) capacity = 2.33kg (average wastewater has 155 - 330mg /L, or 11 - 23 kg / hr)
+        # Limiting factor will then be amount of TSS in the water
 
     def set_input(self, input_pipe):
         self.input = input_pipe
@@ -33,17 +40,29 @@ class SandFilter(GenericPipe.GenericPipe):
         water_height = self.capacity / (math.pi * (self.radius ** 2))
         return math.sqrt(2 * 9.807 * water_height)
 
-    def push(self, flowi_in):
-        self.sand_height += 1 # incrementing amount of particulate in system, should be based on level of particulate in water and flow in
+    def push(self, flow_in):
+        self.particulate_mass += (flow_in / 1000) * 252 # incrementing amount of particulate in system, should be based on level of particulate in water and flow in
         self.max_volume -= math.pi * (self.radius ** 2)
-        self.capacity += flowi_in
+        self.capacity += flow_in
 
-        if self.sand_height > 10: # arbitrary boundary to start backwash
+        if self.particulate_mass > 500000: # arbitrary boundary to start backwash - 500g of particulate collected
             self.backwash = True
             self.output = self.backwash_pipe
             self.input.toggle_valve()
+            self.backwash_timer = 180 / self.tick_length
+        
+        if self.backwash:
+            self.backwash_timer -= 1
+            self.normal_pipe.push(0)
+            if self.backwash_timer == 0:
+                self.backwash = False
+                self.output = self.normal_pipe
+                self.input.toggle_valve()
 
-        flow_out = self.water_velocity() * self.output.get_area() * self.tick_length # get area will get crossectional area of the pipe (or it can be calculated this side)
+        else:
+            self.backwash_pipe.push(0)
+
+        flow_out = self.water_velocity() * self.output.cs_area * self.tick_length # get area will get crossectional area of the pipe (or it can be calculated this side)
         self.output.push(flow_out) # pushes flow out to the output pipe
 
         self.capacity -= flow_out
