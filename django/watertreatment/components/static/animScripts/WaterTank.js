9c4fb3130a2eb1a24a9ebfb3e8f04a782ab9d811
Leo_Lai <leojacklai@outlook.com>
2023-02-26 07:59:27 +0000

    made so that any changes to tank width in code allows the water to match as well

--- django/watertreatment/components/static/animScripts/WaterTank.js
+++ django/watertreatment/components/static/animScripts/WaterTank.js
@@ -2,8 +2,8 @@ import {Fillable} from './Fillable.js';
 import {waterBG} from './AnimController.js';
 
 export class WaterTank extends Fillable {
-  constructor(TLCoord, y, waterWidth, waterHeight, colour, ctx_layer2, tankNum, scada_controller) {
-    super(TLCoord, y, waterWidth, waterHeight, colour);
+  constructor(xCoord, y, waterWidth, waterHeight, colour, ctx_layer2, tankNum, scada_controller) {
+    super(xCoord, y, waterWidth, waterHeight, colour);
 
     // this.currentLevel = (initialLevel- y)/((y+h)-y) // initiallevel normalised to within pixel height of tank
     this.json_list_simdata = JSON.parse(JSON.parse(document.getElementById('all_SimData').textContent));
@@ -15,25 +15,21 @@ export class WaterTank extends Fillable {
     this.scada_controller = scada_controller;
     this.tankNum = tankNum;
 
-    // im sorry this code sucks
     // code for getting flow rates to sync so the next snapshot is reached simultaneously
     const differences = [];
     const json_list = (Object.entries(this.json_list_simdata));
     const next_snapshot_data = json_list.filter((fields) => fields[1]['pk'] === ('tank'+tankNum) && fields[1]['fields']['snapshots'] === this.valueIdx).map((fields) => fields[1]['fields']['waterLevel']);
     const current_snapshot_data = json_list.filter((fields) => fields[1]['pk'] === ('tank'+tankNum) && fields[1]['fields']['snapshots'] === this.valueIdx-1).map((fields) => fields[1]['fields']['waterLevel']);
     const current_difference = Math.abs(current_snapshot_data-next_snapshot_data);
+
     for (let tankNum = 0; tankNum < 4; tankNum++ ) {
       differences.push(scada_controller.change_rate_tank((this.valueIdx-1), tankNum));
     }
+
     this.water_change = current_difference/(Math.max(...differences))/2;
   }
 
   draw() {
-    // rectangle dimensions and positions (for where the draw black square that determines water level)
-    const x = 60 + this.tankNum*200;
-    const y = 210;
-    const width = 80;
-    const height = 153;
     // if the next snapshot water level value is reached move onto the next snapshot
     if (Math.abs(this.currentLevel - this.valuesArr[this.valueIdx]) < 0.00000000001) {
       this.currentLevel = this.valuesArr[this.valueIdx];
@@ -48,9 +44,9 @@ export class WaterTank extends Fillable {
     // if the current snapshot isn't the last update the animation by drawing the water height (this is done via a black square to give illusion of water level dipping/increasing)
     if (this.valueIdx <= this.valuesArr.length) {
       waterBG(this.tankNum);
-      this.ctx_layer2.fillRect(x, y, width, height - this.currentLevel);
+      this.ctx_layer2.fillRect(this.x, this.y, this.w, this.h - this.currentLevel);
       // calls the scada screen controller to sync the current water level of tank with the current water level data displayed on the scada screen
-      this.scada_controller.draw(('tank'+this.tankNum), (this.valueIdx-1), ['component name : '+'tank' + this.tankNum, 'current water lvl : '+this.currentLevel, 'valuesArr :' + this.valuesArr]);
+      this.scada_controller.draw(('tank'+this.tankNum), (this.valueIdx-1), ['component name : '+'tank' + this.tankNum, 'current water lvl : '+ this.currentLevel.toFixed(2), 'valuesArr :' + this.valuesArr]);
     }
   }
 }
