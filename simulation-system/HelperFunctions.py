50d063d1049a8222523642ae54befbfc0bc5eb54
AmosH <vo21765@bristol.ac.uk>
2023-04-20 14:38:43 +0100

    wrote tests for helper functions

--- simulation-system/HelperFunctions.py
+++ simulation-system/HelperFunctions.py
@@ -2,13 +2,13 @@ import FilterSnapshotter
 import FilterSystem
 
 
-def basic_simulation(average_flow, average_tss, sim_length):
+def basic_simulation(average_flow, average_tss, sim_length, testing):
 
     # sim_length is given in seconds, each round is a second, so conversion is direct
     rounds = sim_length
 
     # make and run the simulation, defaults to each round being a second, and snapshotting every round
-    snapshotter = FilterSnapshotter.FilterSnapshotter()
+    snapshotter = FilterSnapshotter.FilterSnapshotter(testing)
     system = FilterSystem.FilterSystem(
         1, average_flow, average_tss, snapshotter, rounds, 1, True)
     system.simulate()
@@ -19,13 +19,13 @@ def basic_simulation(average_flow, average_tss, sim_length):
     # return the name of the table to be used by the server
     return table_name  
 
-def initial_particulate_simulation(average_flow, average_tss, sim_length, initial_particulates):
+def initial_particulate_simulation(average_flow, average_tss, sim_length, initial_particulates, testing):
 
     # sim_length is given in seconds, each round is a second, so conversion is direct
     rounds = sim_length
 
     # make the simulation, using basic 1 snapshot per round, 1 second rounds
-    snapshotter = FilterSnapshotter.FilterSnapshotter()
+    snapshotter = FilterSnapshotter.FilterSnapshotter(testing)
     system = FilterSystem.FilterSystem(1, average_flow, average_tss, snapshotter, rounds, 1, True)
 
     # set initial values to those specified in parameters
