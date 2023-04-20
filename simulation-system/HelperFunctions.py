d064af05a1f0fd814f6fcee17eb8f8d34cf5c944
Lint Action <lint-action@samuelmeuli.com>
2023-04-20 09:20:56 +0000

    Fix code style issues with Autopep8

--- simulation-system/HelperFunctions.py
+++ simulation-system/HelperFunctions.py
@@ -9,11 +9,12 @@ def basic_simulation(average_flow, average_tss, sim_length):
 
     # make and run the simulation, defaults to each round being a second, and snapshotting every round
     snapshotter = FilterSnapshotter.FilterSnapshotter()
-    system = FilterSystem.FilterSystem(1, average_flow, average_tss, snapshotter, rounds, 1, True)
+    system = FilterSystem.FilterSystem(
+        1, average_flow, average_tss, snapshotter, rounds, 1, True)
     system.simulate()
 
     # write results to a database
     table_name = snapshotter.to_database()
 
     # return the name of the table to be used by the server
-    return table_name  
+    return table_name
