db8679382f0b985dc7fcef90926875f529da8420
joshuak08 <joshuakongxw@gmail.com>
2023-04-24 17:23:13 +0100

    tests with mock data done

--- django/watertreatment/components/tests/test_urls.py
+++ django/watertreatment/components/tests/test_urls.py
@@ -1,22 +1,14 @@
-from django.test import TestCase, Client
+from django.test import TestCase, Client, utils
 from django.urls import resolve, reverse
 from components.views import *
 from components.models import *
-from parameterized import parameterized, parameterized_class
 
 
 class TestUrls(TestCase):
-    def setUp(self):
-        self.client = Client()
-        # elements = Document.objects.all()
-        # types = []
-        # for i in elements:
-        #     if i.name not in types:
-        #         types.append(str(i.name))
-        #     else:
-        #         continue
-        # self.types = types
-        # self.elements = elements
+    @classmethod
+    def setUpTestData(cls):
+        cls.data = Document.objects.create(elemID=12345, name='testing',
+                                           params='{"Testing 1" : "1", "Testing 2" : "2"}')
 
     def testHome_at_correct_location(self):
         url = '/'
@@ -49,91 +41,69 @@ class TestUrls(TestCase):
         self.assertTemplateUsed(response, 'components/new-base.html')
         self.assertTemplateUsed(response, 'components/revit-model.html')
 
-    # def createNewDB(self):
+    def testTypes_at_correct_location(self):
+        url = '/types/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'components/types.html')
+        self.assertTemplateUsed(response, 'components/new-base.html')
 
-    # def testTypes_at_correct_location(self):
-    #     url = '/types/'
-    #     response = self.client.get(url)
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertTemplateUsed(response, 'components/types.html')
-    #     self.assertTemplateUsed(response, 'components/new-base.html')
+    def testTypes_url_available_by_name(self):
+        url = reverse('components-types')
+        resolved = resolve(url)
+        response = self.client.get(url)
+        self.assertEqual(resolved.func, types)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'components/new-base.html')
+        self.assertTemplateUsed(response, 'components/types.html')
 
-    # def testTypes_url_available_by_name(self):
-    #     url = reverse('components-types')
-    #     resolved = resolve(url)
-    #     response = self.client.get(url)
-    #     self.assertEqual(resolved.func, types)
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertTemplateUsed(response, 'components/new-base.html')
-    #     self.assertTemplateUsed(response, 'components/types.html')
+    def testElementsOfTypes_url_available_by_name(self):
+        url = reverse('components-elements-of-type', args=[self.data])
+        resolved = resolve(url)
+        response = self.client.get(url)
+        self.assertEqual(resolved.func, elementsOfType)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'components/elements-of-type.html')
+        self.assertTemplateUsed(response, 'components/new-base.html')
 
-    # def testSomething(self):
-    #
+    def testElementsOfTypes_at_correct_location(self):
+        url = '/types/' + self.data.name + '/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'components/elements-of-type.html')
+        self.assertTemplateUsed(response, 'components/new-base.html')
 
-    # Need to figure out how to test each element type page
-    # def testElementsOfTypes_url_available_by_name(self):
-    #     for t in self.types:
-    #         url = reverse('components-elements-of-type', args=[t])
-    #         resolved = resolve(url)
-    #         response = self.client.get(url)
-    #         self.assertEqual(resolved.func, elementsOfType)
-    #         self.assertEqual(response.status_code, 200)
-    #         self.assertTemplateUsed(response, 'components/elements-of-type.html')
-    #         self.assertTemplateUsed(response, 'components/new-base.html')
-    #
-    # def testElementsOfTypes_at_correct_location(self):
-    #     url = '/types/'
-    #     for t in self.types:
-    #         finalUrl = url + t + '/'
-    #         response = self.client.get(finalUrl)
-    #         self.assertEqual(response.status_code, 200)
-    #         self.assertTemplateUsed(response, 'components/elements-of-type.html')
-    #         self.assertTemplateUsed(response, 'components/new-base.html')
-    # #
-    #
-    # # Tests not working
-    # def testElements_at_correct_location(self):
-    #     url = '/types/'
-    #     for t in self.types:
-    #         elements = Document.objects.filter(name=type)
-    #         for i in elements:
-    #             finalUrl = url + t + '/' + str(i.elemID) + '/'
-    #             response = self.client.get(finalUrl)
-    #             self.assertEqual(response.status_code, 200)
-    #             self.assertTemplateUsed(response, 'components/elements.html')
-    #             self.assertTemplateUsed(response, 'components/new-base.html')
-    #
-    # # Tests not working
-    # def testElements_url_available_by_name(self):
-    #     for t in self.types:
-    #         elements = Document.objects.filter(name=type)
-    #         for i in elements:
-    #             print('test')
-    #             url = reverse('components-elements', args=[t, str(i.elemID)])
-    #             resolved = resolve(url)
-    #             response = self.client.get(url)
-    #             print(resolved.func)
-    #             assert 1 == 2
-    #             self.assertEqual(resolved.func, elements)
-    #             self.assertEqual(response.status_code, 200)
-    #             self.assertTemplateUsed(response, 'components/new-base.html')
-    #             self.assertTemplateUsed(response, 'components/elements.html')
+    def testElements_at_correct_location(self):
+        url = '/types/' + self.data.name + '/' + str(self.data.elemID) + '/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'components/element.html')
+        self.assertTemplateUsed(response, 'components/new-base.html')
 
-    # def testSimulation_at_correct_location(self):
-    #     url = '/simulation/'
-    #     response = self.client.get(url)
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertTemplateUsed(response, 'components/simulation.html')
-    #     self.assertTemplateUsed(response, 'components/new-base.html')
-    #
-    # def testSimulation_url_available_by_name(self):
-    #     url = reverse('components-simulation')
-    #     resolved = resolve(url)
-    #     response = self.client.get(url)
-    #     self.assertEqual(resolved.func, simulation)
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertTemplateUsed(response, 'components/new-base.html')
-    #     self.assertTemplateUsed(response, 'components/simulation.html')
+    def testElements_url_available_by_name(self):
+        url = reverse('components-elements', args=[self.data.name, self.data.elemID])
+        resolved = resolve(url)
+        response = self.client.get(url)
+        self.assertEqual(resolved.func, elements)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'components/new-base.html')
+        self.assertTemplateUsed(response, 'components/element.html')
+
+    def testSimulation_at_correct_location(self):
+        url = '/simulation/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'components/simulation.html')
+        self.assertTemplateUsed(response, 'components/new-base.html')
+
+    def testSimulation_url_available_by_name(self):
+        url = reverse('components-simulation')
+        resolved = resolve(url)
+        response = self.client.get(url)
+        self.assertEqual(resolved.func, simulation)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'components/new-base.html')
+        self.assertTemplateUsed(response, 'components/simulation.html')
 
     # Will test when forms merged into dev
     # def testForms(self):
@@ -148,18 +118,3 @@ class TestUrls(TestCase):
         response = resolve(url)
         self.assertEqual(response.func, revitModel)
     """
-
-    # @parameterized([i for i in self.elements])
-    # def TestElements(TestCase):
-    #     def setUp(self):
-    #         self.client = Client()
-    #         elements = Document.objects.all()
-    #         types = []
-    #         for i in elements:
-    #             if i.name not in types:
-    #                 types.append(str(i.name))
-    #             else:
-    #                 continue
-    #         self.types = types
-    #         self.elements = elements
-
