86a9a082b888c29c831089603b2bc25803a1e89c
joshuak08 <joshuakongxw@gmail.com>
2023-04-24 14:19:52 +0100

    using in memory database for tests

--- django/watertreatment/watertreatment/settings.py
+++ django/watertreatment/watertreatment/settings.py
@@ -76,9 +76,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        "TEST": {
-            "MIRROR": "default",
-        },
+        # "TEST": {
+        #     "MIRROR": "default",
+        # },
     },
 
 }
