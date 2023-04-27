658b20ae8811092dc0833c4b6efad99acbe241d0
joshuak08 <joshuakongxw@gmail.com>
2023-04-27 21:29:30 +0100

    redirect tests

--- django/watertreatment/components/tests/test_functional.py
+++ django/watertreatment/components/tests/test_functional.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver import *
 from components.models import *
-from django.urls import reverse
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 import time
 
@@ -10,52 +9,50 @@ from selenium.webdriver.common.by import By
 
 class TestHomePage(StaticLiveServerTestCase):
     host = "127.0.0.1"
-    port = 8000
+    port = 8080
 
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
         options = webdriver.ChromeOptions()
-        # options.add_argument("--headless")
+        options.add_argument("--headless")
         options.add_argument("--window-size=1920,1080")
 
         cls.driver = webdriver.Chrome("chromedriver.exe", options=options)
 
-    # def setUp(self):
-    #     options = webdriver.ChromeOptions()
-    #     # options.add_argument("--headless")
-    #     options.add_argument("--window-size=1920,1080")
-    #     print(options.capabilities)
-    #     self.browser = Chrome('chromedriver.exe', options=options)
-    #     self.browser.service.port = 8000
-
-        # self.live_server_url = "http://127.0.0.1/8000"
-        # print('setup: ' + self.browser.current_url)
-
-    #
-    # @classmethod
-    # def tearDownClass(cls):
-    #     cls.browser.close()
     @classmethod
     def tearDownClass(cls):
         cls.driver.quit()
         super().tearDownClass()
 
 
-    def test_home_page(self):
+    def test_home(self):
+        response = self.driver.get(self.live_server_url)
+        self.assertEqual(self.driver.current_url, "http://127.0.0.1:8080/")
+        self.assertTemplateUsed(response, 'components/new-base.html')
+
+
+    def test_home_types_redirect(self):
         self.driver.get(self.live_server_url)
-        alert = self.driver.find_element(By.CLASS_NAME, "logo")
+        self.driver.find_element(By.XPATH, "/html/body/nav/ul/li[2]/a").click()
+        self.assertEqual(self.driver.current_url, "http://127.0.0.1:8080/types/")
 
-        # alert = self.browser.find_element_by_class_name("logo")
-        self.assertEqual(
-            alert.find_element(By.TAG_NAME, 'p').text,
-            'WATER TREATMENT DIGITAL TWIN'
-        )
+    def test_home_input_form_redirect(self):
+        self.driver.get(self.live_server_url)
+        self.driver.find_element(By.XPATH, "/html/body/nav/ul/li[3]/a").click()
+        self.assertEqual(self.driver.current_url, "http://127.0.0.1:8080/input-form/")
 
-        self.driver.get(self.live_server_url + '/input-form/')
-        time.sleep(2)
-        # print(self.driver.current_url)
+    def test_home_model_redirect(self):
+        self.driver.get(self.live_server_url)
+        self.driver.find_element(By.XPATH, "/html/body/nav/ul/li[4]/a").click()
+        self.assertEqual(self.driver.current_url, "http://127.0.0.1:8080/revit-model/")
 
-    def test_lol(self):
-        # print("second test")
-        self.assertEqual(self.driver.current_url, "http://127.0.0.1:8000/input-form/")
+    def test_home_simulation_redirect(self):
+        self.driver.get(self.live_server_url)
+        self.driver.find_element(By.XPATH, "/html/body/nav/ul/li[5]/a").click()
+        self.assertEqual(self.driver.current_url, "http://127.0.0.1:8080/simulation/")
+
+    def test_home_graph_redirect(self):
+        self.driver.get(self.live_server_url)
+        self.driver.find_element(By.XPATH, "/html/body/nav/ul/li[6]/a").click()
+        self.assertEqual(self.driver.current_url, "http://127.0.0.1:8080/graph/")
