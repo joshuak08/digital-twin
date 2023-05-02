c93cb8160076ec017b8d7c8f426fad780b5eeacd
joshuak08 <joshuakongxw@gmail.com>
2023-05-02 19:06:18 +0100

    Fixed forms range

--- django/watertreatment/components/forms.py
+++ django/watertreatment/components/forms.py
@@ -11,16 +11,16 @@ class SimInputForm(forms.Form):
     # Want to try to create 4 different fields and merge them all into 1 list for particulates in each tank
     tank0 = forms.FloatField(label='Amount of pre-existing particulate in tank 1 [Max value: 11000000] (mg)',
                              validators=[MinValueValidator(0, message="Please enter a valid value larger than 0."),
-                                         MaxValueValidator(1100000, message="Please enter a value smaller than 11000000.")])
+                                         MaxValueValidator(11000000, message="Please enter a value smaller than 11000000.")])
     tank1 = forms.FloatField(label='Amount of pre-existing particulate in tank 2 [Max value: 11000000] (mg)',
                              validators=[MinValueValidator(0, message="Please enter a valid value larger than 0."),
-                                         MaxValueValidator(1100000, message="Please enter a value smaller than 11000000.")])
+                                         MaxValueValidator(11000000, message="Please enter a value smaller than 11000000.")])
     tank2 = forms.FloatField(label='Amount of pre-existing particulate in tank 3 [Max value: 11000000] (mg)',
                              validators=[MinValueValidator(0, message="Please enter a valid value larger than 0."),
-                                         MaxValueValidator(1100000, message="Please enter a value smaller than 11000000.")])
+                                         MaxValueValidator(11000000, message="Please enter a value smaller than 11000000.")])
     tank3 = forms.FloatField(label='Amount of pre-existing particulate in tank 4 [Max value: 11000000] (mg)',
                              validators=[MinValueValidator(0, message="Please enter a valid value larger than 0."),
-                                         MaxValueValidator(1100000, message="Please enter a value smaller than 11000000.")])
+                                         MaxValueValidator(11000000, message="Please enter a value smaller than 11000000.")])
     average_flow = forms.FloatField(label='Average Flow (m^3/s)',
                                     validators=[MinValueValidator(0, message="Please enter a valid value larger than 0."),
                                                 MaxValueValidator(0.5, message="Please enter a value smaller than 0.5")])
