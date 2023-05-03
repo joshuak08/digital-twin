ad9bb81f6bf6b0f383810f60e0ab036c62ba2cee
Lint Action <lint-action@samuelmeuli.com>
2023-05-03 16:44:03 +0000

    Fix code style issues with Autopep8

--- django/watertreatment/simulation-system/HelperFunctionTests.py
+++ django/watertreatment/simulation-system/HelperFunctionTests.py
@@ -9,7 +9,6 @@ class BasicHelperTests(unittest.TestCase):
 
     def test_can_run(self):
         HelperFunctions.basic_simulation(0.3, 252, 10, True)
-        
 
 
 class VariableParticluateTests(unittest.TestCase):
@@ -17,4 +16,3 @@ class VariableParticluateTests(unittest.TestCase):
     def test_can_run(self):
         HelperFunctions.initial_particulate_simulation(
             0.3, 252, 10, [0, 0, 0, 0], True)
-        
